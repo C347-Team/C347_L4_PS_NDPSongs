ffe7f8a707d3e0253dfeca553af8b58631a3e8da
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-05-18 12:56:53 +0800

    implement song class

--- app/src/main/java/sg/edu/rp/c347/id19007966/ndpsongs_ps/Song.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/ndpsongs_ps/Song.java
@@ -1,4 +1,47 @@
 package sg.edu.rp.c347.id19007966.ndpsongs_ps;
 
-public class Song {
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private String title, singers;
+    private int year, stars;
+
+    public Song(String title, String singers, int year, int stars) {
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public void setSingers(String singers) {
+        this.singers = singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
 }
