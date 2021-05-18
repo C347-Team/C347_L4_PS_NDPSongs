ed15e8787715a53698a333a290507ea5804d1da1
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-05-18 17:17:36 +0800

    listview -> modifysongactivity link

--- app/src/main/java/sg/edu/rp/c347/id19007966/ndpsongs_ps/ShowSongsActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/ndpsongs_ps/ShowSongsActivity.java
@@ -1,8 +1,12 @@
 package sg.edu.rp.c347.id19007966.ndpsongs_ps;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,19 +18,50 @@ public class ShowSongsActivity extends AppCompatActivity {
     ArrayAdapter<Song> aa;
     ArrayList<Song> al;
 
+    private int resultKey = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_songs);
-        DBHelper helper = new DBHelper(this);
-        System.out.println(helper.retrieveAll()); // retrieves all
-        System.out.println(helper.retrieveWithConditions(5)); // retrieves only if stars == 5
 
         listView = findViewById(R.id.listView);
 
-        al = helper.retrieveAll();
+        al = getFromDB(false);
         aa = new SongAdapter(this, R.layout.row_song, al);
         listView.setAdapter(aa);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ShowSongsActivity.this, ModifySongActivity.class);
+                intent.putExtra("data", al.get(i));
+                startActivityForResult(intent, resultKey);
+            }
+        });
+    }
+
+    private ArrayList<Song> getFromDB(boolean useStar) {
+        ArrayList<Song> songs;
+        DBHelper helper = new DBHelper(this);
+        if (useStar) {
+            songs = helper.retrieveWithConditions(5);
+        }
+        else {
+            songs = helper.retrieveAll();
+        }
+        helper.close();
+        return helper.retrieveAll();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_OK && resultCode == resultKey) {
+            al.clear();
+            al.addAll(getFromDB(false));
+            aa.notifyDataSetChanged();
+        }
     }
 }
